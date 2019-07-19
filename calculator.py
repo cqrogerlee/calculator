e3463688d513f3db214cdb62dbb830ba973e3955
rogerlee <411339152@qq.com>
2019-07-19 10:59:12 +0800

    3.0 from roger

--- calculator.py
+++ calculator.py
@@ -2,10 +2,16 @@
 支持加减乘除四则运算计算器
 '''
 
-a = input('Enter a number:')
-b = input('Enter an another:')
+while True:
+    a = input("Enter a number or 'q' to quit:")
+    if a.lower()=='q':
+        break
+    b = input("Enter an another or 'q' to quit:")
+    if b.lower()=='q':
+        break
 
-print('a + b =',int(a)+int(b))
-print('a - b =',int(a)-int(b))
-print('a * b =',int(a)*int(b))
-print('a / b =',int(a)/int(b))
+    print('a + b =',int(a)+int(b))
+    print('a - b =',int(a)-int(b))
+    print('a * b =',int(a)*int(b))
+    print('a / b =',int(a)/int(b))
+    
