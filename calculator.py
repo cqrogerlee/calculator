6fd919e670c4e5ee9f8e5d6c49d96550153bb913
rogerlee <411339152@qq.com>
2019-07-19 11:07:32 +0800

    4.0 from roger

--- calculator.py
+++ calculator.py
@@ -1,6 +1,17 @@
 '''
 支持加减乘除四则运算计算器
 '''
+def add(a,b):
+    return a+b
+
+def substract(a,b):
+    return a-b
+
+def multiply(a, b):
+    return a*b
+
+def divide(a, b):
+    return a/b
 
 while True:
     a = input("Enter a number or 'q' to quit:")
@@ -10,8 +21,11 @@ while True:
     if b.lower()=='q':
         break
 
-    print('a + b =',int(a)+int(b))
-    print('a - b =',int(a)-int(b))
-    print('a * b =',int(a)*int(b))
-    print('a / b =',int(a)/int(b))
+    a = int(a)
+    b = int(b)
+
+    print('a + b =',add(a,b))
+    print('a - b =',substract(a,b))
+    print('a * b =',multiply(a,b))
+    print('a / b =',divide(a,b))
     
