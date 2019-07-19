9a5b443df372e46d46fb874d6d878216a6f290a4
rogerlee <411339152@qq.com>
2019-07-19 10:32:30 +0800

    2.0

--- calculator.py
+++ calculator.py
@@ -1,6 +1,11 @@
-#this is the first calculator
+'''
+支持加减乘除四则运算计算器
+'''
 
 a = input('Enter a number:')
 b = input('Enter an another:')
 
 print('a + b =',int(a)+int(b))
+print('a - b =',int(a)-int(b))
+print('a * b =',int(a)*int(b))
+print('a / b =',int(a)/int(b))
